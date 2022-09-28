d9949dfe476939dd432f7a90e9c4b31362cd6030
Angie <angieapazamacotela@gmail.com>
2022-09-28 17:27:59 -0500

    cambio3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
 
 console.log(validator);
+let numerodetarjeta = document.getElementById ("numerodetarjeta");
+let numerodetarjetaNumero = array.from (numerodetarjeta);
+console.log(numerodetarjetaNumero);
+
+var button = document.getElementById ("realizaropago");
+button.addEventListener("click", clicked);
+
+function clicked () {
+    alert ("validotarjeta")
+}
